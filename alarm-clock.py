4ef9998f9491375c79e8c134f3274364671a0fe8
Wesley-nfj <wesleyfonkem7@icloud.com>
2025-08-01 22:03:31 +0100

    Added simple countdown before alarm rings

--- alarm-clock.py
+++ alarm-clock.py
@@ -11,4 +11,10 @@ while True:
     if current_time == alarm_time:
         print("⏰⏰ WAKE UP! It's time!")
         break
+
+     alarm = datetime.strptime(alarm_time, "%H:%M:%S")
+    seconds_left = int((alarm - now.replace(year=alarm.year, month=alarm.month, day=alarm.day)).total_seconds())
+
+    if seconds_left > 0:
+        print(f"{seconds_left} seconds left...", end="\r")
     time.sleep(1)
